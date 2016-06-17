8f3ee6c8df231e46ff6bbb681d61fc6436b022a8
makoto_kw <makoto.kw@gmail.com>
2016-06-17 14:16:25 +0900

    Add recent closed issues block

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_makotokw_mypage_blocks do
   name 'MyPage Blocks by makoto_kw'
   author 'Makoto Kawasaki'
   description 'MyPage blocks for Redmine'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/makotokw/redmine_makotokw_mypage_blocks'
   author_url 'http://makotokw.com'
 end
